b9f9431380b32255f6db08b29f8b5150650e569d
Hayao Mori <hayaomori312@gmail.com>
2025-07-14 20:16:15 +0900

    created tmdb api key & env file

---
+++ react-movie-tutorial/src/services/api.js
@@ -0,0 +1,15 @@
+const API_KEY = process.env.REACT_APP_API_KEY;
+const API_URL = process.env.REACT_APP_API_URL;
+
+export const getPopularMovies = async () => {
+    const response = await fetch(`${API_URL}/movie/popular?api_key=${API_KEY}`);
+    const data = await response.json();
+    return data.results;
+}
+
+export const searchMovies = async (query)  => {
+    const response = await fetch(`${API_URL}/search/movie?api_key=${API_KEY}&query=${encodeURIComponent(query)}`);
+
+    const data = await response.json();
+    return data.results;
+}
