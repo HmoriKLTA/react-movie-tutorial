59f13ad4e41fcff1e91579def3b0f3c62c097fbb
Hayao Mori <hayaomori312@gmail.com>
2025-07-14 17:47:16 +0900

    initial commit

---
+++ react-movie-tutorial/src/pages/Favorites.jsx
@@ -0,0 +1,8 @@
+const favorite =() => {
+    return <div className="favorites-empty">
+        <h2>No Favourite Movies Yet</h2>
+        <p>Start adding movies to your favourites and they will appear here.</p>
+    </div>
+}
+
+export default favorite;
