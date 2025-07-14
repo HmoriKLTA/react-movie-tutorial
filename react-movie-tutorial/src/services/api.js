75743d5223f28bad7b02d0168aa3549b0ecb991c
Hayao Mori <hayaomori312@gmail.com>
2025-07-14 23:21:19 +0900

    finished functionality

--- react-movie-tutorial/src/services/api.js
+++ react-movie-tutorial/src/services/api.js
@@ -1,14 +1,14 @@
-const API_KEY = process.env.REACT_APP_API_KEY;
-const API_URL = process.env.REACT_APP_API_URL;
+const API_KEY = import.meta.env.VITE_API_KEY;
+const BASE_URL = "https://api.themoviedb.org/3";
 
 export const getPopularMovies = async () => {
-    const response = await fetch(`${API_URL}/movie/popular?api_key=${API_KEY}`);
+    const response = await fetch(`${BASE_URL}/movie/popular?api_key=${API_KEY}`);
     const data = await response.json();
     return data.results;
 }
 
 export const searchMovies = async (query)  => {
-    const response = await fetch(`${API_URL}/search/movie?api_key=${API_KEY}&query=${encodeURIComponent(query)}`);
+    const response = await fetch(`${BASE_URL}/search/movie?api_key=${API_KEY}&query=${encodeURIComponent(query)}`);
 
     const data = await response.json();
     return data.results;
